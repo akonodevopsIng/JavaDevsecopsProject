0dc1b320e0402ebe25f2379978e76447c5e87bc1
akonodevopsIng <akono.ibm@gmail.com>
2025-07-05 16:05:33 +0100

    Create main.tf
    Create an Ubuntu (22.04) T2 Large Instance using Terraform

---
+++ main.tf
@@ -0,0 +1,41 @@
+# Provider configuration
+provider "aws" {
+  region = "ap-south-1" # Specify the region
+}
+
+# Create a new security group that allows all inbound and outbound traffic
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_traffic"
+  description = "Security group that allows all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch an EC2 instance
+resource "aws_instance" "my_ec2_instance" {
+  ami             = "ami-0f58b397bc5c1f2e8"
+  instance_type   = "t2.large"
+  key_name        = "MyNewKeyPair"
+  security_groups = [aws_security_group.allow_all.name]
+
+  # Configure root block device
+  root_block_device {
+    volume_size = 30
+  }
+
+  tags = {
+    Name = "MyUbuntuInstance"
+  }
+}
