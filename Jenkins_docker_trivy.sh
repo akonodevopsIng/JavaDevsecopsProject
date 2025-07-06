af597e322d83946d6cc9f8a5986750691270b92b
akonodevopsIng <akono.ibm@gmail.com>
2025-07-06 13:15:17 +0100

    Create Jenkins_docker_trivy.sh

---
+++ Jenkins_docker_trivy.sh
@@ -0,0 +1,27 @@
+# Update packages
+sudo apt update -y
+
+# Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# Install Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+
+# Install Trivy
+sudo apt install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt update -y
+sudo apt install trivy -y
